b439aa536c263f802f7cd716c372cdb4586efd7d
Htet-Myet-Nyein <htetmyetnyein04@gmail.com>
2025-09-29 14:15:32 +0630

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,15 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
+    from collections import defaultdict
     opponent_history.append(prev_play)
-
-    guess = "R"
+    transition_table = defaultdict(lambda: {"R": 0,"P":0,"S":0}
     if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        last_two = "".join(opponent_history[-3:1])
+        transition_table[last_two][opponent_history[-1]] += 1
+        prediction = max(transition_table[last_two], key=transition_table[last_two].get)
+    else:
+        predictiion = "R"
 
-    return guess
+    counter_moves = {"R":"P","P":"S","S":"R"}
+    return counter_moves[prediction]
